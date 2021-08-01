15fadb2b426559d6d3fbd52f1f5ecf74d7e16829
Origho-precious <origho9@gmail.com>
2021-08-01 04:22:04 +0100

    feat: updating Button fixture to use useSelect

--- src/__fixtures__/Button.jsx
+++ src/__fixtures__/Button.jsx
@@ -1,7 +1,15 @@
-import Button  from "../components/Button/Button";
+import { useSelect } from "react-cosmos/fixture";
+import Button from "../components/Button/Button";
 
-export default (
-	<div className="w-60 mx-auto">
-		<Button variant="primary">Cosmos</Button>
-	</div>
-);
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+	const [variant] = useSelect("variant", {
+		options: ["primary", "secondary"],
+	});
+
+	return (
+		<div className="w-60 mx-auto">
+			<Button variant={variant}>Cosmos</Button>
+		</div>
+	);
+};
