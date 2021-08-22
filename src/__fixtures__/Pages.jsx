4e8592d834f8440e132ea2ed3e84327781d3c566
Origho-precious <origho9@gmail.com>
2021-08-22 12:46:25 +0100

    adding a starter template

--- src/__fixtures__/Pages.jsx
+++
@@ -1,8 +0,0 @@
-import Cart from "../pages/cart";
-import Home from "../pages/home";
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  cart: <Cart />,
-  home: <Home />
-};
--- src/__fixtures__/cosmos.decorator.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default ({ children }) => (
-	<div className="bg-gray-100 h-screen">{children}</div>
-);
